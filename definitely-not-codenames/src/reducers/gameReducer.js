6e43ebf0fd7a7d1a76df83a825d9047b0c1e7e45
Matt Meinzer <meinzer.matthew@gmail.com>
2020-05-04 11:44:09 -0500

    Add gameState nested

--- definitely-not-codenames/src/reducers/gameReducer.js
+++ definitely-not-codenames/src/reducers/gameReducer.js
@@ -1,22 +1,14 @@
 import {createReducer} from '@reduxjs/toolkit';
 
 const initialState = {
-  cards: null,
-  turnColor: null,
-  hintWord: null,
-  max: null,
-  guessed: null,
+  gameState: null,
 };
 
 const gameReducer = createReducer(initialState, {
   SET_GAME_STATE: (state, action) => {
     const {game} = action;
 
-    state.cards = game.cards;
-    state.turnColor = game.turnColor;
-    state.hintWord = game.hintWord;
-    state.max = game.max;
-    state.guessed = game.guessed;
+    state.gameState = game;
   },
 });
 
