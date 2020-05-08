a744c1717a5f7f7c2787740bdd3b3d75ba70cd9f
Matt Meinzer <meinzer.matthew@gmail.com>
2020-05-07 19:13:49 -0500

    Make unsassigned hint text black

--- definitely-not-codenames/src/components/HintDisplay.js
+++ definitely-not-codenames/src/components/HintDisplay.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
+import classNames from 'classnames';
 
 const HintDisplay = () => {
   const {clueWord, clueNumber} = useSelector((state) => state.game.gameState);
+  const userColor = useSelector((state) => state.user.team);
   const clueReceived = clueWord && clueNumber >= -1 && clueNumber <= 30;
 
   return (
     <div>
-      <p className="hint__message">
+      <p
+        className={classNames('hint__message', {
+          'hint__message--black': userColor === null,
+        })}
+      >
         {clueReceived ? `${clueWord} ${clueNumber}` : 'awaiting clue...'}
       </p>
     </div>
